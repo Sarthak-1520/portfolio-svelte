85875907eff0f896ef63f6facf5d38c8a9a4a56e
Sarthak Mishra <44714342+Sarthak-1520@users.noreply.github.com>
2024-08-04 04:10:17 +0530

    Added Personal data

--- src/lib/data/home.ts
+++ src/lib/data/home.ts
@@ -3,15 +3,17 @@ import { getSkills } from './skills';
 
 export const title = 'Home';
 
-export const name = 'Name';
+export const name = 'Sarthak';
 
-export const lastName = 'LastName';
+export const lastName = 'Mishra';
 
 export const description =
-	'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti, rerum. Debitis accusantium deleniti enim iste dignissimos? Similique, exercitationem! Odit vero, numquam quae ratione maxime sunt reiciendis laudantium quaerat iure ipsum!';
+	'Welcome to my portfolio Website!';
 
 export const links: Array<{ platform: Platform; link: string }> = [
-	{ platform: Platform.GitHub, link: 'https://github.com/' },
+	{ 
+		platform: Platform.GitHub, 
+		link: 'https://github.com/' },
 	{
 		platform: Platform.Linkedin,
 		link: 'https://www.linkedin.com/'
