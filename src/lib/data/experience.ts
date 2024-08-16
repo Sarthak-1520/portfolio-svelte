df8d65ffbc4b0f90a1cfe941e2c8054f1188cdbf
Sarthak Mishra <44714342+Sarthak-1520@users.noreply.github.com>
2024-08-17 03:58:30 +0530

    Update experience.ts

--- src/lib/data/experience.ts
+++ src/lib/data/experience.ts
@@ -18,36 +18,7 @@ export const items: Array<Experience> = [
 		logo: Assets.Unknown,
 		shortDescription: 'Creating awesome tools for developers.'
 	},
-	{
-		slug: 'software-freelance',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date() },
-		skills: getSkills('svelte', 'ts', 'sass', 'css', 'html', 'js'),
-		name: 'Freelancer',
-		color: 'blue',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
-	},
-	{
-		slug: 'software-freelance-junior',
-		company: 'Self-employed',
-		description: 'Creating awesome applications for customers.',
-		contract: ContractType.Freelance,
-		type: 'Software Development',
-		location: 'Home',
-		period: { from: new Date(2022, 0, 1), to: new Date() },
-		skills: getSkills('css', 'html', 'js'),
-		name: 'Junior Freelancer',
-		color: 'green',
-		links: [],
-		logo: Assets.Unknown,
-		shortDescription: 'Creating awesome applications for customers.'
-	}
+	
 ];
 
 export const title = 'Experience';
